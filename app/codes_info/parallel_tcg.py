1ef44b543bdbfb3323fe683877baf2fa65394895
Harold Azocar <148831545+Harold875@users.noreply.github.com>
2026-04-08 09:35:18 -0400

    feat: include region in Parallel TCG lookup
    - return parallel name and region by code

--- app/codes_info/parallel_tcg.py
+++ app/codes_info/parallel_tcg.py
@@ -9,33 +9,37 @@ class regions(Enum):
 
 parallel = {
     # kathari
-    "CB-371": "Gnaeus Valerus Alpha",
-    "CB-373": "Scipius Magnus Alpha",
-    "CB-375": "Aetio, Exalted Hydrolist",
+    "CB-371": ("Gnaeus Valerus Alpha", regions.kathari),
+    "CB-373": ("Scipius Magnus Alpha", regions.kathari),
+    "CB-375": ("Aetio, Exalted Hydrolist", regions.kathari),
     # augencore
-    "CB-9": "Jahn, Chief Engineer",
-    "CB-376": "Juggernaut Workshop",
-    "CB-21": "Arak, Combat Overseer",
+    "CB-9": ("Jahn, Chief Engineer", regions.augencore),
+    "CB-376": ("Juggernaut Workshop", regions.augencore),
+    "CB-21": ("Arak, Combat Overseer", regions.augencore),
     # earthen
-    "CB-62": "Gaffar, Arbiter of Earth",
-    "CB-389": "Shoshanna, Rebuilder of Earth",
-    "CB-390": "Nehemiah, Defender of Earth",
+    "CB-62": ("Gaffar, Arbiter of Earth", regions.earthen),
+    "CB-389": ("Shoshanna, Rebuilder of Earth", regions.earthen),
+    "CB-390": ("Nehemiah, Defender of Earth", regions.earthen),
     # marcolian
-    "CB-171": "Lemieux, Master Commando",
-    "CB-197": "Catherine Lapointe, The Mad General",
-    "CB-277": "Armored Division HQ",
+    "CB-171": ("Lemieux, Master Commando", regions.marcolian),
+    "CB-197": ("Catherine Lapointe, The Mad General", regions.marcolian),
+    "CB-277": ("Armored Division HQ", regions.marcolian),
     # shroud
-    "CB-379": "Niamh, Wielder of Faith",
-    "CB-380":"The New Dawn",
-    "CB-378": "Brand, Steward Eternal",
+    "CB-379": ("Niamh, Wielder of Faith", regions.shroud),
+    "CB-380": ("The New Dawn", regions.shroud),
+    "CB-378": ("Brand, Steward Eternal", regions.shroud),
 }
 
-def code_info(code: str):
+def code_info(code: str) -> dict[str,str] | None:
     # example code:
     # CB-379,CB-240,3xCB-204,....
     id_parallel = code.split(",")[0]
-    parallel_name = parallel.get(id_parallel, None)
-    return parallel_name
+    parallel_info = parallel.get(id_parallel, None)
+    if parallel_info is None:
+        return None
+    
+    parallel_name, parallel_region = parallel_info
+    return {"paragon": parallel_name, "region": parallel_region.value}
 
 if  __name__ == "__main__":
     result = code_info("CB-379,CB-240,3xCB-204")
